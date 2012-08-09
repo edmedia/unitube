537568a7f2ad6d29c01fefee7f2ea7c1516774bc
Richard Zeng <richard.zeng@otago.ac.nz>
2012-08-10 11:44:18 +1200

    filter to get username from http header (Oracle SSO) only use this filter when deploy to ITS

--- src/main/java/nz/ac/otago/edmedia/media/filter/MediaFilter.java
+++ src/main/java/nz/ac/otago/edmedia/media/filter/MediaFilter.java
@@ -7,55 +7,70 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URLEncoder;
 
 /**
- * Filter for UniTube.
+ * Filter for SSO in ITS.
  *
  * @author Richard Zeng (richard.zeng@otago.ac.nz)
- *         Date: 1/12/2009
+ *         Date: 8/8/2012
  *         Time: 10:57:05
  */
 public class MediaFilter extends AuthenticationFilter {
 
-    private static Logger logger = LoggerFactory.getLogger(MediaFilter.class);
+    private static Logger log = LoggerFactory.getLogger(MediaFilter.class);
+
+    private String ssoHeaderName;
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+        ssoHeaderName = config.getInitParameter("sso.header.name");
+        AppInfo appInfo = new AppInfo();
+        // if allowAccessWithoutLogin is true, user can access protected page, without login
+        // when they do want to login (i.e. to be able to see more information), add parameter "login" with any value to the url
+        appInfo.setAllowAccessWithoutLogin(Boolean.parseBoolean(config.getInitParameter(ALLOW_ACCESS_WITHOUT_LOGIN_INIT_PARAM)));
+        appInfo.setUsingCAS(Boolean.parseBoolean(config.getInitParameter(USING_CAS_AUTHENTICATION_INIT_PARAM)));
+        appInfo.setUsingEmbeddedAuthentication(Boolean.parseBoolean(config.getInitParameter(USING_EMBEDDED_AUTHENTICATION_INIT_PARAM)));
+        if (appInfo.isUsingCAS()) {
+            appInfo.setCasLogoutUrl(config.getInitParameter(LOGOUT_INIT_PARAM));
+            appInfo.setLogoutUrl(config.getInitParameter(BB_LOGOUT_INIT_PARAM));
+            appInfo.setCasCommunicationUrl(config.getInitParameter(COMMUNICATION_INIT_PARAM));
+        }
+        if (appInfo.isUsingEmbeddedAuthentication()) {
+            appInfo.setEmbeddedLoginUrl(config.getInitParameter(EMBEDDED_LOGIN_URL_INIT_PARAM));
+            if (StringUtils.isBlank(appInfo.getEmbeddedLoginUrl()))
+                throw new ServletException("You are using embedded authentication, but didn't provide a login URL.");
+        }
+        appInfo.setInstructors(config.getInitParameter(INSTRUCTORS_INIT_PARAM));
+        appInfo.setStudents(config.getInitParameter(STUDENTS_INIT_PARAM));
+        appInfo.setCourses(config.getInitParameter(COURSES_INIT_PARAM));
+        AuthUtil.setAppInfo(config.getServletContext(), appInfo);
+        log.debug("appInfo = {}", appInfo);
+        if (!appInfo.isUsingCAS() && !appInfo.isUsingEmbeddedAuthentication())
+            throw new ServletException("No authentication method is set! You must set either CAS or Embedded Authentication.");
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        String reqUrl = req.getRequestURI();
-        String query = req.getQueryString();
-        if (StringUtils.isNotBlank(query))
-            reqUrl += "?" + query;
-        if ((reqUrl.contains("/myTube/") || reqUrl.contains("/admin/")) && (req.getParameter("login") == null)) {
-            if (!AuthUtil.isLoggedIn(req)) {
-                AppInfo appInfo = AuthUtil.getAppInfo(req.getSession().getServletContext());
-                String embeddedLoginUrl = appInfo.getEmbeddedLoginUrl();
-                // redirect to login page
-                String url = req.getContextPath();
-                if (!embeddedLoginUrl.startsWith("/"))
-                    url += "/";
-                url += embeddedLoginUrl;
-                if (url.contains("?"))
-                    url += "&from=" + URLEncoder.encode(reqUrl, "UTF-8");
-                else
-                    url += "?from=" + URLEncoder.encode(reqUrl, "UTF-8");
-                logger.info("Redirect to login page: " + url);
-                res.sendRedirect(url);
-                return;
+        String username = AuthUtil.getUserName(req);
+        if (StringUtils.isBlank(username)) {
+            // get username from HTTP header
+            username = req.getHeader(ssoHeaderName);
+            if (StringUtils.isNotBlank(username)) {
+                log.debug("username is not set, set username to {} from HTTP header.", username);
+                AuthUtil.setUserName(req.getSession(), username);
+            } else {
+                log.error("You are not logged in yet.");
+                throw new ServletException("You are not logged in yet.");
             }
         }
-        super.doFilter(request, response, chain);
+        chain.doFilter(request, response);
     }
 
 }
